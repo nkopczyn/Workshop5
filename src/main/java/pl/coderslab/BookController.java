f7c45b916a4866a5a960ea951d127fd4d85fdc13
“nkopczyn” <nkopczyn@visa.com>
2025-08-22 10:37:27 +0200

    mockbookservice methods

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,23 +1,34 @@
 package pl.coderslab;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookController {
+    private final MockBookService mockBookService;
 
-    @RequestMapping("/")
-    public String home() {
-        return "Welcome";
+    public BookController(MockBookService mockBookService) {
+        this.mockBookService = mockBookService;
     }
 
     @RequestMapping("/helloBook")
     public Book helloBook() {
         return new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
+    }
+
+    // pobieranie wszystkich dostępnych książek
+    @RequestMapping("/books")
+    public List<Book> showBooks() {
+        return mockBookService.getBooks();
+    }
+
+    @RequestMapping("/addBook")
+    public void addBook() {
+        mockBookService.add(new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming"));
     }
 }
 
--- src/main/java/pl/coderslab/MockBookService.java
+++ src/main/java/pl/coderslab/MockBookService.java
@@ -1,32 +1,57 @@
 package pl.coderslab;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public class MockBookService implements BookService {
 
-    private List<Book> list;
+    private List<Book> list = new ArrayList<>();
 
     public MockBookService(List<Book> list) {
         this.list = list;
     }
 
+
+    // METHODS
     public List<Book> getBooks() {
-        List<Book> books = new ArrayList<>();
-        return books;
+        return this.list;
     }
 
     public Optional<Book> get(Long id) {
+        for (Book item : list) {
+            if (item.getId().equals(id)) {
+                return Optional.of(item);
+            }
+        }
         return Optional.empty();
     }
 
     public void add(Book book) {
+        list.add(book);
     }
 
     public void delete(Long id) {
+        for (Book item : list) {
+            if (item.getId().equals(id)) {
+                list.remove(item);
+            }
+        }
     }
 
     public void update(Book book) {
+        Long bookId = book.getId();
+        for (Book item : list) {
+            if (item.getId().equals(bookId)) {
+                item.setIsbn(book.getIsbn());
+                item.setTitle(book.getTitle());
+                item.setAuthor(book.getAuthor());
+                item.setPublisher(book.getPublisher());
+                item.setType(book.getType());
+            }
+        }
     }
 }
